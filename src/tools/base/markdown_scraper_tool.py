84952787f7b7f5dffaab39be9c75861e0b599283
aryan-Patel-web <aryanpatel77462@gmail.com>
2026-08-03 00:06:17 +0530

    scraping tool bs4 improvemnrts

--- src/tools/base/markdown_scraper_tool.py
+++ src/tools/base/markdown_scraper_tool.py
@@ -3,6 +3,8 @@ import html2text
 import requests
 from bs4 import BeautifulSoup
 
+
+
 def scrape_website_to_markdown(url: str) -> str:
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36",
